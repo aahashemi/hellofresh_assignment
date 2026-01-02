12f90d2d1b87bc35bec720193e3b0b814769b1f7
aahashemi <aahashemi1380@gmail.com>
2026-01-02 15:18:35 +0100

    add recipes dataset

---
+++ recipes-etl/dataset.py
@@ -0,0 +1,37 @@
+
+import os
+import pandas as pd
+from loguru import logger
+from pathlib import Path
+
+
+class RecipesDataset:
+    DATASET_URL = "https://bnlf-tests.s3.eu-central-1.amazonaws.com/recipes.json"
+    DATASET_FILE_NAME = "recipes.json"
+
+    def download(self, output_file):
+        df = pd.read_json(self.DATASET_URL, lines=True)
+        df.to_json(output_file, orient='records', indent=4)
+        logger.info(f"Dataset downloaded to {output_file}")
+        return
+
+    def load(self, file_path):
+        df =  pd.read_json(file_path)
+        logger.info(f"Dataset loaded from {file_path}")
+        return df
+    
+    def is_downloaded(self, file_path):
+
+        if os.path.exists(file_path):
+            logger.info(f"Dataset found at {file_path}")
+            return True
+        return False
+    
+    def get_dataset(self):
+        output_file = Path(__file__).parent / self.DATASET_FILE_NAME
+
+        if not self.is_downloaded(output_file):
+            self.download(output_file)
+    
+        df = self.load(output_file)
+        return df
